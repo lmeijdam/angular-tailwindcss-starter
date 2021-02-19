50be0aa79441f793f09cc1e1719abeadda6d8b6d
Lars Meijdam <lmeijdam90@gmail.com>
2021-02-19 09:13:29 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-require("dotenv").config();
-const enablePurge = process.env.ENABLE_PURGE || false;
-
 module.exports = {
   purge: {
-    enabled: enablePurge,
+    enabled: true,
     content: ["./src/**/*.html", "./src/**/*.scss"],
   },
   darkMode: false, // or 'media' or 'class'
